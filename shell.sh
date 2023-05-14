a0266900df51b7e4ddde93582dd32a62bbf67cd8
ramesh <rameshshkharat@gmail.com>
2023-05-14 18:08:58 +0530

    A7

--- shell.sh
+++ shell.sh
@@ -4,7 +4,7 @@ sudo date
 sudo df -h
 
 sudo docker ps -a
-docker rm -f $(docker ps -a -q)
+sudo docker rm -f $(docker ps -a -q)
 sudo docker run -d --name=c1 -p 9090:80 docker.io/nginx:latest
 sudo docker ps -a
 sudo curl localhost:9090
