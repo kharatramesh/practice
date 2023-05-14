2524f58e2acaef649e7175ef44a27373e1ea9870
ramesh <rameshshkharat@gmail.com>
2023-05-14 17:30:19 +0530

    A4

--- shell.sh
+++ shell.sh
@@ -2,8 +2,8 @@ echo "Welcome to Linux"
 hostname
 date
 df -h
-apt install docker -y
-docker ps -a
-docker run -d --name=c1 docker.io/httpd:latest
-docker ps -a
 
+docker ps -a
+docker run -d --name=c1 -p 9090:80 docker.io/httpd:latest
+docker ps -a
+curl localhost:9090
